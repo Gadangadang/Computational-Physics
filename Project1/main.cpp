603d333346040d603341ec4f3bceef74f790cafe
Gadangadang <sakarias.frette@outlook.com>
2020-08-29 12:30:01 +0200

    Started project

---
+++ Project1/main.cpp
@@ -0,0 +1,17 @@
+//  Simple matrix inversion example
+#include <iostream>
+#include <new>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+
+int int main(int argc, char const *argv[]) {
+  /* code */
+  // Define matrix size
+  int n = atof(argv[1])
+
+
+
+  return 0;
+}
