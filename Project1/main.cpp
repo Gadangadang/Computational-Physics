605fc21cf971f0cd14ad0b9e4f14d0aed50ac620
Gadangadang <sakarias.frette@outlook.com>
2020-08-29 13:13:37 +0200

    Stash it sakki

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,22 @@
-//  Simple matrix inversion example
+//  Project 1
 #include <iostream>
 #include <new>
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
 #include "armadillo"
+
 using namespace std;
 using namespace arma;
 
 int main(int argc, char const *argv[]) {
   /* code */
+
   // Define matrix size
   int n = atof(argv[1]);
   cout << n << endl;
 
-  // Solve equation Av = b,
+  // Define vectors to solve equation Av = b
   vec v(n);
   vec g(n);
   vec gtilde(n);
