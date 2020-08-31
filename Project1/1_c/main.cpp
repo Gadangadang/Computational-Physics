fe1e7cbed8bff4cdc593a6875dc5eec85b25f62e
Gadangadang <sakarias.frette@outlook.com>
2020-08-31 16:32:22 +0200

    La til mulighet for å løpe over forskjellige n
    La til mulighet for å løpe over forskjellige n

--- Project1/1_c/main.cpp
+++ Project1/1_c/main.cpp
@@ -24,78 +24,82 @@ int main(int argc, char const *argv[]) {
   outfilename = "values.txt";
   ofile.open(outfilename);
   // Define matrix size
-  int n = atof(argv[1]);
-  double h = 1./(n);
-  cout << "Time step :" << h << endl;
-  cout << "Dimension of vectors:" << n << endl;
 
-  // Define vectors to solve equation Av = b
-  vec v(n+1);
-  vec x(n+1);
-  vec g(n+1);
-  vec gtilde(n+1);
-  vec d(n+1);
-  vec dtilde(n+1);
-  vec e(n+1);
-  vec sol(n+1);
-
-
-  for (int i = 0; i<n; i++) e(i) =1;
-  for (int i =0; i<n; i++) d(i) = -2;
-
-  for (int i =0; i<n; i++){
-    g(i) = -h*h*f(i*h);
-    x(i) = i*h;
-  }
-
-
-  dtilde(0) = d(0);
-  gtilde(0) = g(0);
-  v(0)= 0 ;
-  v(n) = 1;
-
-  //Forward Part
-  for (int i = 1; i<n; i++)
+  int ex = atof(argv[1]);
+  for (int i = 1; i <=ex; i++)
   {
-    //i+2 to make sure we get x in [0,1]
+    int n = (int) pow(10,i);
+    double h = 1./(n);
+    cout << "Time step :" << h << endl;
+    cout << "Dimension of vectors:" << n << endl;
+
+    // Define vectors to solve equation Av = b
+    vec v(n+1);
+    vec x(n+1);
+    vec g(n+1);
+    vec gtilde(n+1);
+    vec d(n+1);
+    vec dtilde(n+1);
+    vec e(n+1);
+    vec sol(n+1);
 
 
-    dtilde(i) = d(i)-1./dtilde(i-1);
-    gtilde(i) = g(i) - gtilde(i-1)/(dtilde(i-1));
-  }
-  //Backward Part
+    for (int i = 0; i<n; i++) e(i) =1;
+    for (int i =0; i<n; i++) d(i) = -2;
 
-  v(n-1) = gtilde(n-1)/dtilde(n-1);
-
-  //exact solution
-
-  for (int i= 0; i <n; i++){
-    sol(i) = exactfunc(i*h);
-  }
-
-  for (int i = n-2; i>0; i--)
-  {
-    v(i) = (gtilde(i) - v(i+1))/dtilde(i);
-
-  }
-
-  for (int i = 0; i<n; i++){
-    ofile << setprecision(15) << v(i);
-    ofile << setprecision(15) << x(i);
-    ofile << setprecision(15) << sol(i);
-
-    if (i == 0){ //correct for nan value in first error calc
-      ofile << setprecision(15) << 0 << endl;
-    }
-    else{
-      ofile << setprecision(15) << relativeerror(v(i),sol(i)) << endl;
+    for (int i =0; i<n; i++){
+      g(i) = -h*h*f(i*h);
+      x(i) = i*h;
     }
 
 
-  }
+    dtilde(0) = d(0);
+    gtilde(0) = g(0);
+    v(0)= 0 ;
+    v(n) = 1;
 
-  ofile.close()
+    //Forward Part
+    for (int i = 1; i<n; i++)
+    {
+      //i+2 to make sure we get x in [0,1]
 
+
+      dtilde(i) = d(i)-1./dtilde(i-1);
+      gtilde(i) = g(i) - gtilde(i-1)/(dtilde(i-1));
+    }
+    //Backward Part
+
+    v(n-1) = gtilde(n-1)/dtilde(n-1);
+
+    //exact solution
+
+    for (int i= 0; i <n; i++){
+      sol(i) = exactfunc(i*h);
+    }
+
+    for (int i = n-2; i>0; i--)
+    {
+      v(i) = (gtilde(i) - v(i+1))/dtilde(i);
+
+    }
+
+    for (int i = 0; i<n; i++){
+      ofile << setprecision(15) << v(i);
+      ofile << setprecision(15) << x(i);
+      ofile << setprecision(15) << sol(i);
+
+      if (i == 0){ //correct for nan value in first error calc
+        ofile << setprecision(15) << 0 << endl;
+      }
+      else{
+        ofile << setprecision(15) << relativeerror(v(i),sol(i)) << endl;
+      }
+
+
+    }
+
+    ofile.close()
+    }
 
   return 0;
 }
