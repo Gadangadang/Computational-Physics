bc58e60082b712960c41a5297fefea73980d0711
Gadangadang <sakarias.frette@outlook.com>
2020-08-31 10:28:36 +0200

    Fiksa plottekode, og gjort ferdig 1_b
    Fiksa plottekode, og gjort ferdig 1_b. Nå trenger vi bare kjøre make all fil så kjører den python også

--- Project1/1_b/Plotte_code.py
+++ Project1/1_b/Plotte_code.py
@@ -5,48 +5,35 @@ data = open('valn0.txt', 'r')
 y_value = []
 x_value = []
 
+data1 = open('valn1.txt', 'r')
+y1_value = []
+x1_value = []
+
+data2 = open('valn2.txt', 'r')
+y2_value = []
+x2_value = []
+
 for line in data:
     y_value.append(float(line.split(' ')[0]))
     x_value.append(float(line.split(' ')[1]))
 
+for line in data1:
+    y1_value.append(float(line.split(' ')[0]))
+    x1_value.append(float(line.split(' ')[1]))
+
+for line in data2:
+    y2_value.append(float(line.split(' ')[0]))
+    x2_value.append(float(line.split(' ')[1]))
+
 plt.plot(x_value,y_value,"r-", label="n=10")
+plt.plot(x1_value, y1_value,"b^", label="n=100" )
+plt.plot(x2_value, y2_value,"g--",label="n=1000" )
+
 plt.xlabel('x=(0,1)')
 plt.ylabel('u(x)')
 plt.legend()
 plt.show()
 
 data.close()
-
-data1 = open('valn1.txt', 'r')
-y1_value = []
-x1_value = []
-
-for line in data1:
-    y_value.append(float(line.split(' ')[0]))
-    x_value.append(float(line.split(' ')[1]))
-
-plt.plot(x1_value, y1_value,"bo", label="n=100" )
-plt.xlabel('x=(0,1)')
-plt.ylabel('u(x)')
-plt.legend()
-plt.show()
-
 data1.close()
-
-data2 = open('valn2.txt', 'r')
-y2_value = []
-x2_value = []
-
-for line in data2:
-    y_value.append(float(line.split(' ')[0]))
-    x_value.append(float(line.split(' ')[1]))
-
-print(y2_value)
-
-plt.plot(x2_value, y2_value,"g--",label="n=1000" )
-plt.xlabel('x=(0,1)')
-plt.ylabel('u(x)')
-plt.legend()
-plt.show()
-
 data2.close()
