dce048fd2bf3c41eac4d32af0cec49704ea44392
William Hirst <bod_holthe@outlook.com>
2020-09-03 13:54:15 +0200

    DB
    Har fikset 1_e slik at vi får en god tilnærming for n=100 og n=1000.

--- Project1/1_e/main2.cpp
+++ Project1/1_e/main2.cpp
@@ -24,8 +24,6 @@ inline double exactfunc(double x){return 1-(1-exp(-10))*x-exp(-10*x);}
 int main(int argc, char const *argv[]) {
   /* code */
 
-  // Define matrix size
-
   int ex = atof(argv[1]);
 
   for (int i=1; i<= ex; i++){
@@ -49,27 +47,26 @@ int main(int argc, char const *argv[]) {
 
     }
 
-
-
     int n = (int) pow(10,i);
-    double h = 1./(n);
+    n = n; //Reset n to only use end points
+    double h = 1./(n+1);
     cout << "Time step :" << h << endl;
     cout << "Dimension of vectors:" << n << endl;
 
     // Define vectors to solve equation Av = b
-    n = n-1; //Reset n to only use end points
     mat A = zeros<mat>(n,n);
     vec b(n);
     vec v(n);
     vec x(n);
     vec exac(n);
+    vec Y(n);
+    vec X(n);
     clock_t start, finish;
     start = clock();
-    A(0,0) = -2; A(0,1) = 1; x(0) = h; b(0) = -h*h*f(x(0));
-    x(n-1) = x(0) + (n-1)*h; b(n-1) = -h*h*f(x(n-1));
-
+    A(0,0) = -2; A(0,1) = 1; x(0) = 0; b(0) = -h*h*f(x(0));
+    x(n-1) = 1; b(n-1) = -h*h*f(x(n-1));
     for (int i =1; i<n-1; i++){
-      x(i) = x(0) + i*h;
+      x(i) = (i)*h;
       b(i) = -h*h*f(x(i));
       A(i,i-1) = 1;
       A(i,i) = -2;
@@ -77,20 +74,20 @@ int main(int argc, char const *argv[]) {
 
     }
     A(n-1,n-1) = -2; A(n-1,n-2) = 1; A(n-2,n-1) = 1;
+
+    mat P, L, U;
+    lu(L,U,P,A);
     //Solve Av = b
-    v = solve(A,b);
+    X = solve(trimatu(U), solve(trimatl(L), P*b) );
     finish = clock();
     double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
-    cout << setprecision(10) << "N="<< n+1<< ":  Time used  for computing=" << timeused  << endl;
+    cout << setprecision(10) << "N="<< n<< ":  Time used  for computing=" << setprecision(3) << timeused  << endl;
     for (int i = 0; i<n; i++){
-      exac(i) = exactfunc(i*h);
-      ofile << setprecision(15) << v(i) << " " << x(i) << " " << exac(i) << endl;
+      exac(i) = exactfunc((i+1)*h);
+      ofile << setprecision(15) << X(i) << " " << x(i) << " " << exac(i) << endl;
     }
     ofile.close();
   }
-
-
-
 
   return 0;
 }
