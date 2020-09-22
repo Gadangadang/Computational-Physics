6163a29b552322f6812f8ee3902546840d64fb01
William Hirst <bod_holthe@outlook.com>
2020-09-22 13:29:42 +0200

    Simple de-buggs
    Fjernet et par {}, osv.

--- Project2/2_b/classtuff.cpp
+++ Project2/2_b/classtuff.cpp
@@ -50,9 +50,6 @@ void classtuff::offdiag(mat A, int *p, int *q, int n){
 
 }
 
-  }
-}
-}
 
 void classtuff::Rotate(mat A, mat S, int p, int q, int n){
   /*
@@ -106,7 +103,6 @@ void classtuff::Rotate(mat A, mat S, int p, int q, int n){
 }
 
 
-}
 void classtuff::Jacobi(mat A, int maxiter, double eps){
   double nde_m;
   int iter, p, q, n;
@@ -115,7 +111,7 @@ void classtuff::Jacobi(mat A, int maxiter, double eps){
   iter = 0;
   while( nde_m > eps && iter <= maxiter){
     offdiag(A,&p, &q, n);
-    Jacobi_rotate(A, R, p, q, n);
+    Jacobi_rotate(A, S, p, q, n);
     nde_m = A(p,q);
     iter ++;
   }
