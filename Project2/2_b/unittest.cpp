09b2034fae04a37a97239c465fddd6f2eda75a2e
Gadangadang <sakarias.frette@outlook.com>
2020-09-23 15:12:46 +0200

    dd Unit test working
    Unit test is now fully operational

--- Project2/2_b/unittest.cpp
+++ Project2/2_b/unittest.cpp
@@ -5,24 +5,38 @@
 #include "armadillo"
 using namespace arma ;
 
+inline void offfdiag(mat A, int &p, int &q, int n, double &maxoff){
+  maxoff=0;
+  for(int i = 0; i<n; ++i){
+    for(int j = 0;  j < n; ++j){
+            double aij = fabs(A(i, j));
+            if(aij > maxoff && i !=j){
+              maxoff = aij; p = i; q = j;
+      }
+    }
+  }
+}
 
-mat A = zeros<mat>(3,3);
-classtuff test_obj ;
 
-mat Atest = test_obj.Initialize(3,A);
-test_obj.offdiag(Atest, p,q,n,maxoff);
 
 TEST_CASE( "Check for errors in code" ) {
+  int p,q;
+  double maxoff;
+
+  mat A = zeros<mat>(3,3);
+  classtuff test_obj ;
+
+  mat Atest = test_obj.Initialize(3,A);
+  offfdiag(Atest, p,q,3,maxoff);
 
   SECTION("Finding max value of matrix"){
-    REQUIRE( test_obj.maxoff == 1 );
+    REQUIRE( maxoff == 1 );
   }
 
+  vec test_eigvals = test_obj.Jacobi_arm(Atest);
+  mat qen = test_obj.Jacobi(Atest,1e-16);
+
   SECTION("Finding eigenvalues for matrix"){
-
-    vec test_eigvals = test_obj.Jacobi_arm(Atest);
-    mat qen = test_obj.Jacobi(Atest,1e-16);
-
     REQUIRE( fabs(accu(sort(test_eigvals) - sort(qen.diag()))) < 1e-3 );
   }
 
