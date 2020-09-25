18a465a6778081cbec7882da0c3e43baf4d24614
Anders <Anders@eduroam-193-157-237-11.wlan.uio.no>
2020-09-25 14:41:22 +0200

    Oppdaterte enhetstester til de nye funksjonene.

--- Project2/unittest.cpp
+++ Project2/unittest.cpp
@@ -4,23 +4,24 @@
 #include "classtuff.hpp"
 #include "armadillo"
 using namespace arma ;
-
+double V(double x){return 0;}
 TEST_CASE( "Check for errors in code" ) {
   int p,q;
   double maxoff;
 
   mat A = zeros<mat>(3,3);
   classtuff test_obj ;
-
-  mat Atest = test_obj.Initialize(3,A);
+  double a = 0;
+  double b = 1;
+  int c_size = 3; 
+  mat Atest = test_obj.Initialize(a, b, V, c_size);
   test_obj.offdiag(Atest, p,q,3,maxoff);
 
   SECTION("Finding max value of matrix"){
-    REQUIRE( maxoff == 1 );
+    REQUIRE( maxoff == 16 );
   }
-
   vec test_eigvals = test_obj.Jacobi_arm(Atest);
-  mat qen = test_obj.Jacobi(Atest,1e-16);
+  mat qen = test_obj.Jacobi(Atest, 1e-16, c_size);
 
   SECTION("Finding eigenvalues for matrix"){
     REQUIRE( fabs(accu(sort(test_eigvals) - sort(qen.diag()))) < 1e-3 );
