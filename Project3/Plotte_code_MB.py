3a85f4bdf0d93a4e83748ccee79110fb9c2763fe
William Hirst <bod_holthe@outlook.com>
2020-10-16 14:08:37 +0200

    change color of sun

--- Project3/Plotte_code_MB.py
+++ Project3/Plotte_code_MB.py
@@ -45,7 +45,7 @@ u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
 x = r*np.cos(u)*np.sin(v)
 y = r*np.sin(u)*np.sin(v)
 z = r*np.cos(v)
-ax.plot_wireframe(x, y, z, color="r")
+ax.plot_wireframe(x, y, z, color="y")
 
 ax.set_xlim3d([-np.max(p[1,:,0]),np.max(p[1,:,0])])
 ax.set_ylim3d([-np.max(p[1,:,1]),np.max(p[1,:,1])])
