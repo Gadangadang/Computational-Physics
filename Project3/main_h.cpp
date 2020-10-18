344a5d580bd629ade6d2afd1fad7374e6f4dead4
William Hirst <bod_holthe@outlook.com>
2020-10-18 09:26:23 +0200

    Found bug when defining initial vels
    The Nasa data was in AU/day not AU/year. Now it works. Also added Mars.

--- Project3/main_h.cpp
+++ Project3/main_h.cpp
@@ -25,28 +25,28 @@ int main(int argc, char const *argv[]) {
 
 
   int IntegrationPoints = 50000;
-  double FinalTime = .5;
+  double FinalTime = 10;
 
   //double TimeStep = FinalTime/((double) IntegrationPoints);
   double earth_mass = 3.003e-6;
   double sun_mass = 1.0;
   double jup_mass = 0.0009543;
+  double mar_mass = 3.213e-7;
   int fixed =1;
-  vec vel(3);vel={-6.957269992142644*1e-03, 1.579734315560513*1e-02, -2.582593092148153*1e-07};
-  vec vel2(3);vel2={6.442741439253338*1e-03, 4.130146620372741*1e-03, -1.612738541610256*1e-04};
-  vec vel3(3);vel3 = {-7.280593132276730*1e-06,-5.090234498858063*1e-06,2.181619304215098*1e-07};
-  //vec vels(3);vels = (-vel2*jup_mass-vel*earth_mass)/sun_mass;
+
   double beta = 2;
   cout << "Beta " << beta << endl;
   //Earth
-  object planet1(earth_mass,9.128884513088843*1e-01,3.928032801600736*1e-01,6.577938183713410*1e-05,-6.957269992142644*1e-03, 1.579734315560513*1e-02, -2.582593092148153*1e-07);
+
+  object planet1(earth_mass,9.128884513088843*1e-01,3.928032801600736*1e-01,6.577938183713410*1e-05,-6.957269992142644*1e-03*365, 1.579734315560513*1e-02*365, -2.582593092148153*1e-07*365);
   //Jupiter
-  object planet2(jup_mass,2.556653950007264,-4.428596022378350,-3.882840438937561*1e-02,6.442741439253338*1e-03, 4.130146620372741*1e-03, -1.612738541610256*1e-04);
+  object planet2(jup_mass,2.556653950007264,-4.428596022378350,-3.882840438937561*1e-02,6.442741439253338*1e-03*365, 4.130146620372741*1e-03*365, -1.612738541610256*1e-04*365);
   //Sun
-  object planet3(sun_mass, -6.107925513172998*1e-03,6.420679726598624*1e-03,8.893727401374147*1e-05,-7.280593132276730*1e-06,-5.090234498858063*1e-06,2.181619304215098*1e-07);
-  //object planet3(sun_mass, -6.107925513172998*1e-03,6.420679726598624*1e-03,8.893727401374147*1e-05,vels[0],vels[1],vels[2]);
+  object planet3(sun_mass, -6.107925513172998*1e-03,6.420679726598624*1e-03,8.893727401374147*1e-05,-7.280593132276730*1e-06*365,-5.090234498858063*1e-06*365,2.181619304215098*1e-07*365);
+  //Mars
+  object planet4(mar_mass,1.308532937474490E+00,5.379930853282286E-01,-2.102141505220228E-02,-4.717434819666467E-03*365,1.416299295671162E-02*365,4.126758221879923E-04*365);
   solving binary_verlet(5.0);
-  binary_verlet.add(planet1); binary_verlet.add(planet2);binary_verlet.add(planet3);
+  binary_verlet.add(planet1); binary_verlet.add(planet2);binary_verlet.add(planet3); binary_verlet.add(planet4);
   binary_verlet.VelocityVerlet(Dimension,IntegrationPoints,FinalTime,1,0., beta, fixed);
 
   return 0;
