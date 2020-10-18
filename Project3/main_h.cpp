6f182933fc890c8c779a45bef7d5ba475e987ebb
William Hirst <bod_holthe@outlook.com>
2020-10-18 09:34:01 +0200

    Added Saturn to main_h

--- Project3/main_h.cpp
+++ Project3/main_h.cpp
@@ -32,6 +32,7 @@ int main(int argc, char const *argv[]) {
   double sun_mass = 1.0;
   double jup_mass = 0.0009543;
   double mar_mass = 3.213e-7;
+  double sat_mass = 0.0002857;
   int fixed =1;
 
   double beta = 2;
@@ -45,8 +46,10 @@ int main(int argc, char const *argv[]) {
   object planet3(sun_mass, -6.107925513172998*1e-03,6.420679726598624*1e-03,8.893727401374147*1e-05,-7.280593132276730*1e-06*365,-5.090234498858063*1e-06*365,2.181619304215098*1e-07*365);
   //Mars
   object planet4(mar_mass,1.308532937474490E+00,5.379930853282286E-01,-2.102141505220228E-02,-4.717434819666467E-03*365,1.416299295671162E-02*365,4.126758221879923E-04*365);
+  //Saturn
+  object planet5(sat_mass,5.144031493282123E+00,-8.563703043404734E+00,-5.588847142609978E-02,4.471296615234172E-03*365,2.858910024157302E-03*365,-2.280276562541721E-04*365);
   solving binary_verlet(5.0);
-  binary_verlet.add(planet1); binary_verlet.add(planet2);binary_verlet.add(planet3); binary_verlet.add(planet4);
+  binary_verlet.add(planet1); binary_verlet.add(planet2);binary_verlet.add(planet3); binary_verlet.add(planet4);binary_verlet.add(planet5);
   binary_verlet.VelocityVerlet(Dimension,IntegrationPoints,FinalTime,1,0., beta, fixed);
 
   return 0;
