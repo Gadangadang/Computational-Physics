32ec70b30dd36f897db8a613973bc9443bb7d016
William Hirst <bod_holthe@outlook.com>
2020-11-20 13:38:20 +0100

    Improved plots for d
    Now compares ordered and not ordered for each temp.

--- Project4/CodeBase/main_d.cpp
+++ Project4/CodeBase/main_d.cpp
@@ -22,13 +22,14 @@ int main(int argc, char* argv[])
    clock_t start, finish;
    start = clock();
    double L = 20.;
-   double T = 2.4;
-   int mcs_max = 1e5;
-   int param_1 = 1.;
-   Mcint1.Initialize(L, mcs_max,T,param_1);
-   string filename = "MonteCarloRun.txt";
-   Mcint1.init_output(filename);
-   Mcint1.MonteCarloV1();
+   int mcs_max = 1e4;
+   string filename1 = "T_1_o.txt"; string filename2 = "T_2_o.txt";
+   string filename3 = "T_1_n.txt"; string filename4 = "T_2_n.txt";
+
+   Mcint1.Initialize(L, mcs_max,1.,1);Mcint1.init_output(filename1);Mcint1.MonteCarloV1();
+   Mcint1.Initialize(L, mcs_max,2.4,1);Mcint1.init_output(filename2);Mcint1.MonteCarloV1();
+   Mcint1.Initialize(L, mcs_max,1.,0.);Mcint1.init_output(filename3);Mcint1.MonteCarloV1();
+   Mcint1.Initialize(L, mcs_max,2.4,0.);Mcint1.init_output(filename4);Mcint1.MonteCarloV1();
    finish = clock();
    double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
    cout << setprecision(10) << "Time used  for computing (single thread) = " << timeused  << " Seconds"<<endl;
