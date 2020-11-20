ebafa5befa11a83d18a5c0420fd4934ddade2eb1
anders556 <anders556@live.no>
2020-11-20 12:45:53 +0100

    parallelized g)

--- Project4/CodeBase/main_g.cpp
+++ Project4/CodeBase/main_g.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include "time.h"
 #include <stdio.h>
+#include <omp.h>
 #include <tuple>
 #include <cmath>
 // polluting the namespaces
@@ -18,12 +19,6 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-   solver Mcint1;
-
-   Mcint1.init_output();
-
-   clock_t start, finish;
-
 
    double L1 = 40.; double L2 = 60.; double L3 = 80.; double L4 = 100.;
 
@@ -35,23 +30,22 @@ int main(int argc, char* argv[])
    int param = 0;
 
 
-   start = clock();
+   double start = omp_get_wtime();
+   omp_set_num_threads(100); // this number needs to be optimized for individual pc's !
+   #pragma omp parallel for
    for (double i_temp = init_temp; i_temp <= final_temp; i_temp += t_step){
-
+     solver Mcint1;
+     Mcint1.init_output();
      Mcint1.Initialize(L1, mcs, i_temp, param);
      string filename = "MCL40.txt";
      Mcint1.MonteCarloV2(filename);
    }
-   finish = clock();
-   double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
+   double finish = omp_get_wtime();
+   double timeused = (double) (finish - start);
    cout << setprecision(10) << "Time used  for computing (single thread) = " << timeused  << " Seconds"<<endl;
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 2df9d6ea2a81e45549d824a4782dc8517ebf8491
 
 
 
