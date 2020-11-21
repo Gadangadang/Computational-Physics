843d0eee39e545ccbd9afe1773b0ca39ffd9fe07
Gadangadang <sakarias.frette@outlook.com>
2020-11-21 16:04:56 +0100

    Added interpolating code, tested om current data

--- Project4/CodeBase/tcfind.py
+++ Project4/CodeBase/tcfind.py
@@ -73,7 +73,7 @@ cv40 = cv40[tindex]
 t = t[tindex]
 
 spl40 = UnivariateSpline(t, cv40)
-spl40.set_smoothing_factor(0.65)
+spl40.set_smoothing_factor(0.61)
 
 #Cv60
 t6 = np.asarray(t6)
@@ -83,7 +83,7 @@ cv60 = cv60[t6index]
 t6 = t6[t6index]
 
 spl60 = UnivariateSpline(t6, cv60)
-spl60.set_smoothing_factor(2)
+spl60.set_smoothing_factor(5)
 
 #Cv80
 t8 = np.asarray(t8)
@@ -93,7 +93,7 @@ cv80 = cv80[t8index]
 t8 = t8[t8index]
 
 spl80 = UnivariateSpline(t8, cv80)
-spl80.set_smoothing_factor(5)
+spl80.set_smoothing_factor(10.8)
 
 #Cv100
 t1 = np.asarray(t1)
@@ -103,23 +103,22 @@ cv100 = cv100[t1index]
 t1 = t1[t1index]
 
 spl100 = UnivariateSpline(t1, cv100)
-spl100.set_smoothing_factor(4)
-"""
-plt.scatter(t, cv40, label="C_v 40")
+spl100.set_smoothing_factor(13)
+
+#plt.scatter(t, cv40, label="C_v 40")
 plt.plot(t,spl40(t),"r",label="C_v 40 Univeriate spline")
 
 
-plt.scatter(t6, cv60, label="C_v 60")
-plt.plot(t6,spl60(t6),"r",label="C_v 60 Univeriate spline")
+#plt.scatter(t6, cv60, label="C_v 60")
+plt.plot(t6,spl60(t6),"k",label="C_v 60 Univeriate spline")
 
 
-plt.scatter(t8, cv80, label="C_v 80")
-plt.plot(t8,spl80(t8),"r",label="C_v 80 Univeriate spline")
-"""
+#plt.scatter(t8, cv80, label="C_v 80")
+plt.plot(t8,spl80(t8),"brown",label="C_v 80 Univeriate spline")
 
-plt.scatter(t1, cv100, label="C_v 100")
-plt.plot(t1,spl100(t1),"r",label="C_v 100 Univeriate spline")
 
+#plt.scatter(t1, cv100, label="C_v 100")
+plt.plot(t1,spl100(t1),"green",label="C_v 100 Univeriate spline")
 
 plt.legend()
 plt.xlabel("T [J]")
@@ -135,23 +134,24 @@ Interpolate to find vals
 """
 
 
-"""
-cv40max = np.max( spl40 )
-cv60max = np.max( spl60 )
-cv80max = np.max( spl80 )
-cv100max = np.max( spl100 )
+
+cv40max = np.max( spl40(t) )
+cv60max = np.max( spl60(t6) )
+cv80max = np.max( spl80(t8) )
+cv100max = np.max( spl100(t1) )
 
 
-t40index = int(np.where(cv40max == cv40 )[0])
+t40index = int(np.where(cv40max == spl40(t) )[0][0])
+
 t40max = t[t40index]
 
-t60index = int(np.where(cv60max == cv60 )[0])
+t60index = int(np.where(cv60max == spl60(t6) )[0][0])
 t60max = t[t60index]
 
-t80index = int(np.where(cv80max == cv80 )[0])
+t80index = int(np.where(cv80max == spl80(t8) )[0][0])
 t80max = t[t80index]
 
-t100index = int(np.where(cv100max == cv100 )[0])
+t100index = int(np.where(cv100max == spl100(t1) )[0][0])
 t100max = t[t100index]
 
 
@@ -166,5 +166,4 @@ tcinf_80 = t80max - amean*80**(-1)
 tcinf_100 = t100max - amean*100**(-1)
 
 Tcin_mean = (tcinf_40 + tcinf_60 + tcinf_80 + tcinf_100)/4
-print(Tcin_mean)
-"""
+print("Tc at infinity is {:.3f}".format(Tcin_mean))
