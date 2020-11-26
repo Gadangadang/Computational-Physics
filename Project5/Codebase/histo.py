6a1fc5c46473f17d189d598ef1b3949bc6b0ca71
Gadangadang <sakarias.frette@outlook.com>
2020-11-26 13:02:57 +0100

    Added pesky taxes

--- Project5/Codebase/histo.py
+++ Project5/Codebase/histo.py
@@ -7,9 +7,9 @@ for line in data:
     Mon.append(x1)
 num_bins = len(Mon)
 Mon = np.asarray(Mon)
-plt.axvspan(0, 200, facecolor='saddlebrown', alpha=0.5, label="Peasants (from Borgen)") # X coordinate
-plt.axvspan(200, 600, facecolor='silver', alpha=0.5, label="Middle class (from Toten)") # X coordinate
-plt.axvspan(600, np.max(Mon), facecolor='gold', alpha=0.5, label="Upper class (from Stavanger)") # X coordinate
+plt.axvspan(0, 0.2*np.max(Mon), facecolor='saddlebrown', alpha=0.5, label="Peasants (from Borgen)",) # X coordinate
+plt.axvspan(np.max(Mon)*0.2, 0.95*np.max(Mon), facecolor='silver', alpha=0.5, label="Middle class (from Toten)") # X coordinate
+plt.axvspan(np.max(Mon)*0.95 , np.max(Mon), facecolor='gold', alpha=0.5, label="Upper class (from Stavanger)") # X coordinate
 
 n, bins, patches = plt.hist(Mon, 200, facecolor='b')
 #plt.axhspan(0, np.max(np.asarray(n)), facecolor='0.2', alpha=0.5) #Y coordinate
