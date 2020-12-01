ad053857ad43531fb7630b4d13e52aa8e27eec03
William Hirst <bod_holthe@outlook.com>
2020-12-01 11:07:18 +0100

    Imported necaccery libraries

--- Project5_BS/Black_scholes.cpp
+++ Project5_BS/Black_scholes.cpp
@@ -1,1 +1,18 @@
+#include "armadillo"
+#include "Black_scholes.hpp"
+#include <iostream>
+#include <new>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include "time.h"
+#include <stdio.h>
+#include <tuple>
+#include <cmath>
+#include <stdlib.h>
 
+// polluting the namespaces
+using namespace arma;
+using namespace std;
--- Project5_BS/Black_scholes.hpp
+++ Project5_BS/Black_scholes.hpp
@@ -1,1 +1,18 @@
+#ifndef SOLVER_HPP
+#define SOLVER_HPP
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "armadillo"
+#include <string>
 
+using namespace arma;
+using namespace std;
+
+class Black_scholes
+{
+    private:
+
+    public:
+};
+
+#endif // ISING_MCINT
