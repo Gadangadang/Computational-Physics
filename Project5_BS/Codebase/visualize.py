15f7a2292addc9f7acac53bc310d457d7410735e
Gadangadang <sakarias.frette@outlook.com>
2020-12-02 15:43:27 +0100

    Added analytical solution to visualize.py

--- Project5_BS/Codebase/visualize.py
+++ Project5_BS/Codebase/visualize.py
@@ -43,7 +43,7 @@ def d1(S_t,ti):
     global sigma
     global E
 
-    return 1/(sigma*np.sqrt(np.max(t)-ti)) * ( np.log(S_t/E) + (r + sigma**2/2)*(np.max(t)-ti))  
+    return 1/(sigma*np.sqrt(np.max(t)-ti)) * ( np.log(S_t/E) + (r + sigma**2/2)*(np.max(t)-ti))
 
 def d2(S_t, ti):
     return d1(S_t, ti) -sigma*np.sqrt(ti)
@@ -52,12 +52,12 @@ def N(d):
     return (special.erf(d/np.sqrt(2)) + 1)/np.sqrt(2)
 
 def Vana(S_t,ti):
-    return N( d1(S_t, ti) )*S_t - N(d2(S_t,ti))*E*np.exp(r*ti)
-"""
-for i in range(len(t)):
-    plt.plot(S,Vana(),label="V(S,t={})".format(t[i]))
+    return N( d1(S_t, ti) )*S_t - N(d2(S_t,ti))*E*np.exp(-r*ti)
+
+for i in range(len(t)-1):
+    plt.plot(S[1:-1],Vana(S[1:-1],t[i]),label="V(S,t={})".format(t[i]))
 
 plt.legend()
 plt.xlabel("Price of underlying asset")
 plt.ylabel("Value of option")
-plt.show()"""
+plt.show()
