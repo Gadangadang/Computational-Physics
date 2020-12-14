29b45560940cfeae4f5b199a9f327ae45ac5b063
Gadangadang <sakarias.frette@outlook.com>
2020-12-14 12:47:57 +0100

    Readmatrix func added to unit test

--- Project5_BS/CodeBase/Unittest.cpp
+++ Project5_BS/CodeBase/Unittest.cpp
@@ -16,6 +16,60 @@
 using namespace arma ;
 using namespace std ;
 
+vector<vector<double> > readMatrix(string filename){
+
+  // File pointer
+  fstream fin;
+  // Open an existing file
+  fin.open(filename, ios::in);
+  // Read the Data from the file
+  // as String Vector
+  vector<double> S;
+  vector<double> time;
+  string line, word, temp;
+
+  int count = 0;
+  vector<vector<double> > matrix; //Matrix to hold option value for each time step
+
+  while (fin >> temp){
+
+    getline(fin, line);
+
+    // used for breaking words
+    stringstream s(line);
+
+    std::string resultstr = s.str();
+    const char* cstr2 = resultstr.c_str();
+
+    vector <double> VperS;
+
+    char* line = (char*) cstr2;
+    char* pch = strtok(line, " ");
+    if (count == 0){  //If count == 0, line in file is stock price
+      while (pch != NULL){
+        S.push_back( std::atof(pch) );
+        pch = strtok (NULL, " ");
+      }
+      count += 1;
+    }
+    else{ //Time and option value
+      int hello = 0;
+      while (pch != NULL){
+        if ( hello == 0 ){
+          time.push_back( std::atof(pch) );
+          pch = strtok (NULL, " ");
+          hello += 1;
+        }
+        VperS.push_back( std::atof(pch) );
+        pch = strtok (NULL, " ");
+      }
+      matrix.push_back(VperS);
+    }
+  }
+
+  return matrix;
+}
+
 TEST_CASE( "Check for errors in code" ) {
 
 
@@ -29,54 +83,8 @@ TEST_CASE( "Check for errors in code" ) {
     SC.Initialize(T,X,N,filename,r,D,sigma,E);
     SC.Crank_Nic();
 
-    // File pointer
-    fstream fin;
-    // Open an existing file
-    fin.open("u.txt", ios::in);
-    // Read the Data from the file
-    // as String Vector
-    vector<double> S;
-    vector<double> time;
-    string line, word, temp;
 
-    int count = 0;
-    vector<vector<double> > matrix; //Matrix to hold option value for each time step
-
-    while (fin >> temp){
-
-      getline(fin, line);
-
-      // used for breaking words
-      stringstream s(line);
-
-      std::string resultstr = s.str();
-      const char* cstr2 = resultstr.c_str();
-
-      vector <double> VperS;
-
-      char* line = (char*) cstr2;
-      char* pch = strtok(line, " ");
-      if (count == 0){  //If count == 0, line in file is stock price
-        while (pch != NULL){
-          S.push_back( std::atof(pch) );
-          pch = strtok (NULL, " ");
-        }
-        count += 1;
-      }
-      else{ //Time and option value
-        int hello = 0;
-        while (pch != NULL){
-          if ( hello == 0 ){
-            time.push_back( std::atof(pch) );
-            pch = strtok (NULL, " ");
-            hello += 1;
-          }
-          VperS.push_back( std::atof(pch) );
-          pch = strtok (NULL, " ");
-        }
-        matrix.push_back(VperS);
-      }
-    }
+    vector<vector<double> > matrix = readMatrix(filename);
 
     vector<double> compare_vec;
     for (int i = 0; i < N; i++){
@@ -95,5 +103,8 @@ TEST_CASE( "Check for errors in code" ) {
   }
   SECTION("Tridiag works as expected"){
     REQUIRE(1 == 1);
+  }
+  SECTION(""){
+
   }
 }
