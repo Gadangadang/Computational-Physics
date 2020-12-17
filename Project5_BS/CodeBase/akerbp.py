cfb1126aec8bb6a8013a4722125453c0dad62197
William Hirst <bod_holthe@outlook.com>
2020-12-17 14:21:01 +0100

    Added plot of main_d
    Added some labels and tweeked some parameters.

--- Project5_BS/CodeBase/akerbp.py
+++ Project5_BS/CodeBase/akerbp.py
@@ -30,26 +30,23 @@ for line in data:
         rest_list.append(float(element))
 
     V2.append(rest_list)
-
+plt.axvspan(S1[-1]-0.1, S1[-1]+0.1,edgecolor='k',facecolor='None',alpha=1, label="Date:15.12, Stock price ={}".format(S1[-1]))
+plt.axvspan(S1[-2]-0.1, S1[-2]+0.1,edgecolor='g',facecolor='None',alpha=1, label="Date:14.12, Stock price ={}".format(S1[-2]))
+plt.axvspan(S1[-3]-0.1, S1[-3]+0.1,edgecolor='r',facecolor='None',alpha=1, label="Date:11.12, Stock price ={}".format(S1[-3]))
+plt.axvspan(S1[-4]-0.1, S1[-4]+0.1,edgecolor='b',facecolor='None',alpha=1, label="Date:10.12, Stock price ={}".format(S1[-4]))
+plt.legend()
 S1 = np.asarray(S1)
-time = ['10.12','11.12','14.12','15.12']
-ac_tim = [-1,-2,-5,-6]
-plt.scatter(S1,np.asarray(V1),label='Reele options verdier')
-for i, txt in enumerate(time):
-    plt.annotate(txt, (S1[i], V1[i]))
-
+ac_tim = [len(t)-1,len(t)-2,len(t)-3,len(t)-4]
+plt.scatter(S1,np.asarray(V1),label='Real options values')
 V_num=[]
 S_num = []
-for j in range(len(time)):
+for j in range(4):
     indx = int(np.where(np.abs(S1[j]-S2)==min(np.abs(S1[j]-S2)))[0][0])
     S_num.append(S2[indx])
-    V_num.append(V2[int(-j-1)][indx])
-plt.scatter(S_num,V_num,label='Numeriske options verdier')
-for i, txt in enumerate(time):
-    plt.annotate(txt, (S_num[i], V_num[i]))
-
-
+    V_num.append(V2[int(ac_tim[j])][indx])
+plt.scatter(S_num,V_num,label='Numerical option values')
 plt.legend()
-plt.xlabel('Stock-prizes')
-plt.ylabel('Option-vals')
+plt.xlabel('Stock-prizes (S)' ,fontsize=12)
+plt.ylabel('Option-values (V)' ,fontsize=12)
+plt.title('Numerical vs Analytical option-values for Aker BP',fontsize=12)
 plt.show()
