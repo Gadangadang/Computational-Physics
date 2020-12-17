af951eb5b5ffbef3b980a1176e09d2de6e8be17a
William Hirst <bod_holthe@outlook.com>
2020-12-17 16:20:40 +0100

    Removed Projekt5_finac from our map.

--- Project5/Codebase/main_c.cpp
+++ Project5/Codebase/main_c.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "armadillo"
-#include "Finance.hpp"
+#include "Black_scholes.hpp"
 #include <new>
 #include <cstdio>
 #include <cstdlib>
@@ -11,28 +11,18 @@
 #include <stdio.h>
 #include <tuple>
 #include <cmath>
-// polluting the namespaces
+
 using namespace arma;
 using namespace std;
-
+//This main is used to calculate data to visualize the greeks.
 int main(int argc, char* argv[])
 {
-   int mcs = 1.5e5;
-   int L = 500;
-   double m_0 = 100;
-   double tax_or_no = 0;
-   double min_tax = 0;
-   double alpha = 0;
-   string filename1 = "Savings025.txt";
-   string filename2 = "Savings05.txt";
-   string filename3 = "Savings09.txt";
-   Finance Fc;
-   double savings025 = 0.25;double savings05 = 0.5;double savings09 = 0.9;
+   Black_scholes SC;
    clock_t start, finish;
    start = clock();
-   Fc.Initialize(mcs, L,m_0,filename1,tax_or_no,min_tax,savings025,alpha,0);Fc.MonteCarlo();Fc.print_vec(filename1);
-   Fc.Initialize(mcs, L,m_0,filename2,tax_or_no,min_tax,savings05,alpha,0);Fc.MonteCarlo();Fc.print_vec(filename2);
-   Fc.Initialize(mcs, L,m_0,filename3,tax_or_no,min_tax,savings09,alpha,0);Fc.MonteCarlo();Fc.print_vec(filename3);
+   vec sigma_vec =linspace(0.1,1.,1e2); vec r_vec = linspace(0.01,0.5,1e2);
+   string rfilename = "greeks_r.txt";string sfilename = "greeks_s.txt";
+   SC.Greeks(sigma_vec,r_vec, rfilename, sfilename);
    finish = clock();
    double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
    cout << setprecision(10) << "Time used  for computing (single thread) = " << timeused  << " Seconds"<<endl;
--- Project5/Codebase/main_d.cpp
+++ Project5/Codebase/main_d.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "armadillo"
-#include "Finance.hpp"
+#include "Black_scholes.hpp"
 #include <new>
 #include <cstdio>
 #include <cstdlib>
@@ -11,30 +11,23 @@
 #include <stdio.h>
 #include <tuple>
 #include <cmath>
-// polluting the namespaces
+
 using namespace arma;
 using namespace std;
-
+//This main is used to calcualte option-values for a spesific S and \tau
+//so that we can approximate reel sigma.
 int main(int argc, char* argv[])
 {
-
-   int L = 1024;
-   double m_0 = 1;
-   int mcs = 1e5;
-   double tax_or_no = 0;
-   double min_tax = 0;
-   double alpha05 = 0.5;double alpha1 = 1.;double alpha15 = 1.5;double alpha2 = 2.;
-   string filename2 = "V_vis.txt";
-   string alpha_05="alpha_05.txt"; string alpha_1="alpha_1.txt";
-   string alpha_15="alpha_15.txt";string alpha_2="alpha_2.txt";
-   Finance Fc;
-   double savings025 = 0;
+   Black_scholes SC;
    clock_t start, finish;
    start = clock();
-   Fc.Initialize(mcs, L,m_0,filename2,tax_or_no,min_tax,savings025,alpha05);Fc.MonteCarlo();Fc.print_vec(alpha_05);
-   Fc.Initialize(mcs, L,m_0,filename2,tax_or_no,min_tax,savings025,alpha1);Fc.MonteCarlo();Fc.print_vec(alpha_1);
-   Fc.Initialize(mcs, L,m_0,filename2,tax_or_no,min_tax,savings025,alpha15);Fc.MonteCarlo();Fc.print_vec(alpha_15);
-   Fc.Initialize(mcs, L,m_0,filename2,tax_or_no,min_tax,savings025,alpha2);Fc.MonteCarlo();Fc.print_vec(alpha_2);
+   double T = 6.; double X=0.25; int N=1e4;
+   int print_per =6;
+   double D =(1.83989+1.85169+2.02683+5.4525)/((double)217.20);
+   string filename="Aker_numeric.txt";double r = 0.0011; double sigma=0.3; double E=215.;
+   SC.Initialize(T,X,N,filename,r,D,sigma,E);
+   //SC.D1d_explicit();
+   SC.Crank_Nic(print_per);
    finish = clock();
    double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
    cout << setprecision(10) << "Time used  for computing (single thread) = " << timeused  << " Seconds"<<endl;
